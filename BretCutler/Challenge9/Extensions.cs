1e15836d4b9d372d2abe5cbd509dea73445aba08
bcutler1 <bcutler1@yahoo.com>
2015-02-04 08:14:24 -0700

    Add Challenge 9

---
+++ BretCutler/Challenge9/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge9 {
+
+    public static class Extensions {
+
+        public static bool IsFibonacci(this long val) {
+
+            long first = 0;
+            long second = 1;
+            long fib = first + second;
+
+            while (fib < val) {
+
+                first = second;
+                second = fib;
+                fib = first + second;
+            }
+
+            return fib == val;
+
+        }
+    }
+}
---
+++ BretCutler/Challenge9/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Challenge9 {
+    public partial class frmMain : Form {
+        public frmMain() {
+            InitializeComponent();
+        }
+
+        private void cmdCalc_Click(object sender, EventArgs e) {
+
+            lstResults.DataSource = GetResults(txtValues.Text);
+        }
+
+        private List<string> GetResults(string p) {
+
+            List<string> rtn = new List<string>();
+
+            string[] values = p.Split(',');
+
+            for (int i = 0; i < values.Length; i++) {
+                try {
+
+                    long val = long.Parse(values[i]);
+
+                    rtn.Add(string.Format("{0} - {1}", val, (val.IsFibonacci()) ? "IS Fibonacci" : "NOT Fibonacci"));
+
+                } catch (Exception ex) {
+                    rtn.Add(string.Format("{0} - Not Valid Entry", values[i]));
+                }
+            }
+
+            return rtn;
+        }
+    }
+}
